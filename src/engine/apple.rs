378989fb88e52c8b152e4d4d45c691301cbe43b6
devitodbug <davidochiengy@gmail.com>
2026-06-25 12:16:46 +0300

    feat: update random generator to a version that generates f32

--- src/engine/apple.rs
+++ src/engine/apple.rs
@@ -1,31 +1,26 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*, rand::gen_range};
 
 use crate::engine::helpers;
 
-struct Apple {
+pub struct Apple {
     x: f32,
     y: f32,
 }
 
 impl Apple {
-    fn new() -> Self {
-        Self { x: 0.0, y: 0.0 }
+    pub fn new() -> Self {
+        Self {
+            x: gen_range(0.0, screen_width()),
+            y: gen_range(0.0, screen_height()),
+        }
     }
 
-    fn _reset_position(&mut self) {
-        let max_width = screen_width();
-        let max_height = screen_height();
-        let max_width_i32 = max_width as i32;
-        let max_height_i32 = max_height as i32;
-
-        let x = rand::gen_range(1, max_width_i32);
-        let y = rand::gen_range(1, max_height_i32);
-
-        self.x = x as f32;
-        self.y = y as f32;
+    pub fn reset_position(&mut self) {
+        self.x = gen_range(0.0, screen_width());
+        self.y = gen_range(0.0, screen_height());
     }
 
-    fn _render_apple(self) {
+    pub fn render_apple(&self) {
         draw_rectangle(
             self.x,
             self.y,
